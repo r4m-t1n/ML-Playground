dafffbf783cb496143fea48ddc77154116552558
r4m-t1n <im.r4mt1n@gmail.com>
2025-07-09 23:57:25 +0330

    chore: complete the code

--- backend/models/simple_regression.py
+++ backend/models/simple_regression.py
@@ -1,4 +1,3 @@
-import torch
 import torch.nn as nn
 
 class SimpleLinearRegression(nn.Module):
@@ -10,19 +9,16 @@ class SimpleLinearRegression(nn.Module):
     def forward(self, x):
         return self.fc(x)
 
-def training_loop(
+def training_loop_slr(
     x_train, y_train,
-    x_val, y_val,
     model, optimizer, criterion,
     epochs):
 
-    for epoch in range(1, epochs+1):
+    for _ in range(1, epochs+1):
         output_train = model(x_train)
         loss_train = criterion(output_train, y_train)
         optimizer.zero_grad()
         loss_train.backward()
         optimizer.step()
         
-        with torch.no_grad():
-            output_val = model(x_val)
-            loss_val = criterion(output_val, y_val)
+    return loss_train.item()
