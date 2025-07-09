ef30707a066e20a340998f46bcecca6201c76c50
r4m-t1n <im.r4mt1n@gmail.com>
2025-07-09 23:57:55 +0330

    feat: add simple classifier

---
+++ backend/models/simple_classifier.py
@@ -0,0 +1,24 @@
+import torch
+import torch.nn as nn
+
+class SimpleClassifier(nn.Module):
+    def __init__(self, input_f=2, output_f=1):
+        super().__init__()
+        self.linear = nn.Linear(input_f, output_f)
+    
+    def forward(self, x):
+        return torch.sigmoid(self.linear(x))
+
+def training_loop_classifier(
+    x_train, y_train,
+    model, optimizer, criterion,
+    epochs):
+
+    for _ in range(1, epochs + 1):
+        output_train = model(x_train)
+        loss_train = criterion(output_train, y_train)
+        optimizer.zero_grad()
+        loss_train.backward()
+        optimizer.step()
+        
+    return loss_train.item()
