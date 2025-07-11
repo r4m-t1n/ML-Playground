282ceaad2577c6c338c59a0ccf9ae9b39ab0b222
r4m-t1n <im.r4mt1n@gmail.com>
2025-07-11 23:48:48 +0330

    feat: add pydantic

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
-import streamlit as st
+from pydantic import BaseModel
 
 from models.simple_regression import SimpleLinearRegression, training_loop_slr
 from models.simple_classifier import SimpleClassifier, training_loop_classifier
@@ -19,6 +19,31 @@ models = {
     "mlp-regression": MLPRegression(),
 }
 
+class DataLR(BaseModel):
+    x: float
+    y: float
+
+class DataClassifier(BaseModel):
+    x: float
+    y: float
+    label: int
+
+class TrainRequestLR(BaseModel):
+    data: list[DataLR]
+    learning_rate: float
+    epochs: int
+
+class TrainRequestClassifier(BaseModel):
+    data: list[DataClassifier]
+    learning_rate: float
+    epochs: int
+
+class TrainRequestMLP(BaseModel):
+    data: list[DataLR]
+    learning_rate: float
+    epochs: int
+    hidden_size: int
+
 @app.get("/", response_class=HTMLResponse)
 async def root(request: Request):
     return templates.TemplateResponse(
@@ -26,8 +51,7 @@ async def root(request: Request):
     )
 
 app.post("/train_simple_linear_regression")
-
-async def train_simple_classifier():
+async def train_simple_linear_regression():
     pass
 
 app.post("/train_simple_classifier")
